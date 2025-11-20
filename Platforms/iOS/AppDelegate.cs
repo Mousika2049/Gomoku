78ac3ebad9910304763629b0aabeebfe7faac5ff
scorp <scorp@DESKTOP-379MQJ4>
2025-11-20 10:44:38 +0800

    添加项目文件。

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GomokuAI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
